3288c0660a47723ea58339be3e3588a5b00991cb
SkyLite / SkyNet <hkim6000@gmail.com>
2026-03-07 14:20:59 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////////////
+//  prerequisite : install - view terminal & Edit project file
+//  - dotnet add package Microsoft.Data.SqlClient
+//  - dotnet add package System.Drawing.Common
+//  - Add option to Properties/launchsetting.json file  : "hotReloadEnabled": false
+//////////////////////////////////////////////////////////
+
+using SkyNet;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//////////////////////////////////////////////////////////
+builder.Services.AddHttpContextAccessor();    //1.Add HttpContext Service
+//////////////////////////////////////////////////////////
+
+var app = builder.Build();
+
+//////////////////////////////////////////////////////////
+app.UseMiddleware<IHandler>();  //2. use SKYNET.IHANDLER as middleware service
+app.UseStaticHttpCurrent();    //3. use static http class service
+//////////////////////////////////////////////////////////
+
+app.Run();
+
